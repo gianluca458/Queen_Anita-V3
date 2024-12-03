602e5bbf24eae8ee80eadf914e77b1e56b839c54
gianluca458 <richardkilunju5@gmail.com>
2024-12-03 15:48:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 const { color } = require('./lib/myfunc')
 
 //owner
-global.owner = '2347043759577'
-global.nomerowner = ["2347043759577"]
+global.owner = '25470754641980'
+global.nomerowner = ["254754641980"]
 
 // Apikey 
 
